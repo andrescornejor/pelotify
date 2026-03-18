bc3d4e0caece526659a60decac37ea320458ba65
AndresCornejoReitich <andycornejo2002@gmail.com>
2026-03-17 23:18:28 -0300

    saco adnroid

--- amateur-football-mvp/android/app/src/main/java/com/fulbo/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.fulbo.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- amateur-football-mvp/android/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.fulbo.app',
-  appName: 'fulbo',
-  webDir: 'wwww.localhost:3000'
-};
-
-export default config;
