14fa7a273fee2c022937f16c103b2b7dfc7b1d4f
AndresCornejoReitich <andycornejo2002@gmail.com>
2026-03-19 20:51:32 -0300

    Update ThemeContext.tsx

--- amateur-football-mvp/src/contexts/ThemeContext.tsx
+++ amateur-football-mvp/src/contexts/ThemeContext.tsx
@@ -21,7 +21,7 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
     if (savedTheme) {
       setTheme(savedTheme);
     } else {
-      setTheme('system');
+      setTheme('dark');
     }
   }, []);
 
