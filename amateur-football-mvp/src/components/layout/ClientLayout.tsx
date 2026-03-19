97c3910bd5a7897d495495db933fe54ca2056804
AndresCornejoReitich <andycornejo2002@gmail.com>
2026-03-19 20:34:10 -0300

    Update ClientLayout.tsx

--- amateur-football-mvp/src/components/layout/ClientLayout.tsx
+++ amateur-football-mvp/src/components/layout/ClientLayout.tsx
@@ -47,7 +47,7 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
         <main className={cn(
           'flex-1 w-full relative',
           showNav
-            ? pathname.startsWith('/match/')
+            ? pathname.startsWith('/match')
               ? 'pb-24 lg:pb-12'
               : 'pt-24 lg:pt-24 pb-24 lg:pb-12'
             : ''
