01a98125d32ea2e9b5e26b21a9745fc02a65a47c
AndresCornejoReitich <andycornejo2002@gmail.com>
2026-03-30 00:30:52 -0300

    Update constants.ts

--- amateur-football-mvp/src/lib/constants.ts
+++ amateur-football-mvp/src/lib/constants.ts
@@ -1,11 +1,10 @@
-export const AVAILABLE_TIMES = Array.from({ length: 35 }).map((_, i) => {
-  // Start from 7:00 (i=0 -> 7:00, i=1 -> 7:30)
-  const totalMinutes = 7 * 60 + i * 30;
+export const AVAILABLE_TIMES = Array.from({ length: 18 }).map((_, i) => {
+  // Start from 7:00 (i=0 -> 7:00, i=1 -> 8:00)
+  const totalMinutes = 7 * 60 + i * 60;
   const hours = Math.floor(totalMinutes / 60) % 24;
-  const minutes = totalMinutes % 60 === 0 ? '00' : '30';
-  return `${hours.toString().padStart(2, '0')}:${minutes}`;
+  return `${hours.toString().padStart(2, '0')}:00`;
 });
-// 35 steps of 30 mins from 07:00:
+// 18 steps of 60 mins from 07:00:
 // 0: 07:00
 // ...
-// 34: 07:00 + (34 * 30) = 07:00 + 17:00 = 24:00 (00:00)
+// 17: 07:00 + 17:00 = 24:00 (00:00)
