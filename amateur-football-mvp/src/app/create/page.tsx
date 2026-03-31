de2fc5be7efa618d874477451a8c746093fe1c21
AndresCornejoReitich <andycornejo2002@gmail.com>
2026-03-31 12:27:40 -0300

    Update page.tsx

--- amateur-football-mvp/src/app/create/page.tsx
+++ amateur-football-mvp/src/app/create/page.tsx
@@ -817,7 +817,6 @@ export default function CreateMatchPage() {
                         })
                       )}
                     </div>
-                    
                   </div>
                 </motion.div>
               )}
