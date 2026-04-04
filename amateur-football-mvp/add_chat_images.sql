4a56e961a587081d76550482a2690906dc15f853
AndresCornejoReitich <andycornejo2002@gmail.com>
2026-04-04 15:33:10 -0300

    Update add_chat_images.sql

--- amateur-football-mvp/add_chat_images.sql
+++ amateur-football-mvp/add_chat_images.sql
@@ -5,3 +5,4 @@ ALTER TABLE public.direct_messages ADD COLUMN IF NOT EXISTS image_url TEXT;
 -- Create storage bucket for chat images if it doesn't exist
 -- Note: Supabase storage buckets are usually created via the dashboard or API,
 -- but we can try to hint at it or use a default 'uploads' bucket.
+--------
